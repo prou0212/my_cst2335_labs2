eb15e8e44e2c155dfb0b2023ecdd01726e9c9c7d
Jesse Proulx <prou0212@algonquinlive.com>
2025-06-04 12:41:32 -0400

    Finally done

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
     String? verifyLogin = prefs.getString("savedLogin");
     String? verifyPassword = prefs.getString("savedPassword");
     if (verifyLogin != null && verifyPassword != null) {
-      print("✅ Successfully saved both login and password!");
+      print("Successfully saved both login and password!");
       print("Login: $verifyLogin");
       print("Password $verifyPassword");
     }
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           SnackBar(content: Text("Login data loaded and filled in TextFields!")),
         );
       });
-      print("✅ Both login and password loaded into TextFields");
+      print("Both login and password loaded into TextFields");
     } else {
       print("No saved data found");
     }
