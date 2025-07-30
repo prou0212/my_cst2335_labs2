301538a76f36d1dd97621a933aa52b4142eb06ba
Jesse Proulx <prou0212@algonquinlive.com>
2025-07-30 12:16:59 -0400

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Lab 7'),
+      home: const MyHomePage(title: 'Lab 8'),
     );
   }
 }
